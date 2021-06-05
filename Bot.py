78af8b74f4fed2a6c8864187576419f37ee500df
Hrituraj Kalsekar <66247693+jacktheripp3r@users.noreply.github.com>
2021-06-05 10:36:35 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,11 +7,7 @@
 import tweepy
 import requests
 import base64
-import bs4
 import time
-from binance.client import Client
-from datetime import timedelta, datetime
-from dateutil import parser
 import locale
 import os 
 from os import environ 
@@ -20,17 +16,12 @@ from os import environ
 # In[2]:
 
 
-binance_api_key = environ['binance_api_key']    #Enter your own API-key here
-binance_api_secret = environ['binance_api_secret'] #Enter your own API-secret here
-
-client = Client(api_key=binance_api_key, api_secret=binance_api_secret)
-
-
 api = environ['api']
 api_secret = environ['api_secret']
 api_bearer = environ['api_bearer']
 accesstoken = environ['accesstoken']
 accesssecrettoken = environ['accesssecrettoken']
+cmcapi = environ['cmcapi']
 
 
 # In[3]:
@@ -41,99 +32,58 @@ auth=tweepy.OAuthHandler(api,api_secret)
 auth.set_access_token(accesstoken,accesssecrettoken)
 api=tweepy.API(auth)
 
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
-# In[4]:
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': cmcapi,
+}
 
-
-y = 0
-listone = []
-while y < 10:
-    url = requests.get('https://www.coingecko.com/en')
-    soup = bs4.BeautifulSoup(url.text, 'lxml')
-    scrapedsymbol = soup.select('.d-lg-none.font-bold')[y].text
-    scrapedsymbol = scrapedsymbol.rstrip("\n")
-    scrapedsymbol = scrapedsymbol.strip("\n")
-    y += 1
-    listone.append(scrapedsymbol)
-
-
-# In[31]:
-
-
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
-iteri = 0
-percent = 0
-lp = True
 while True:
-    tweetlist = []
-    latestprice = client.get_all_tickers()
-    for tick in listone:
-
-        firsthalf = tick
-
-        concat = firsthalf + 'USDT'
-        
-        if 'USD' in firsthalf:
-            #print(firsthalf + ': $1')
-            iteri += 1
-            tweet = firsthalf + ': $1'
-            tweetlist.append(tweet)
+    tweet = ''
+    tweet2 = ''
+    for i in range(10):
+        cryptoprice = round((response.json()['data'][i]['quote']['USD']['price']), 2)
+        price = response.json()['data'][i]['quote']['USD']['price']
+        change = response.json()['data'][i]['quote']['USD']['percent_change_1h']
+        valuechange = round(price * change / 100, 2)
+        if 'USD' in response.json()['data'][i]['symbol']:
+            tweet = tweet + str(i+1) + '.' + response.json()['data'][i]['symbol'] + ': $1' +'\n'
             continue
-
-        if iteri < 10:
-            a = client.get_all_tickers()
-            x = 0
-            for i in a:
-                if a[x]['symbol'] == concat:
-                    #print('If this is true')
-                    #print(x)
-                    p = round(float(a[x]['price']), 2)
-                    #p = round(p, 2)
-                    tweet = firsthalf + ': $' + str(p)
-                    tweetlist.append(tweet)
-                    break
-                x += 1
-                
-            #api.update_status(final)
-            iteri += 1
+        if change > 0:
+            add = str(i+1) + '.' + response.json()['data'][i]['symbol'] + ': {}(+{}, {}%)'.format(locale.currency(cryptoprice, grouping=True), locale.currency(valuechange, grouping = True), round(change, 2)) + '\n'
+            if len(tweet+add) > 280:
+                tweet2 = tweet2 + add
+            else:
+                tweet = tweet + add
         else:
-            #latestprice = client.get_all_tickers()
-            x = 0
-            for i in latestprice:
-                if latestprice[x]['symbol'] == concat:
-                    p = float(latestprice[x]['price'])
-                    p = round(p, 2)
-                    price = firsthalf + ': ' + locale.currency(p, grouping=True)
-                    change = float(p) - float(a[x]['price'])
-                    change = round(change, 4)
-                    percent = round(change/float(a[x]['price']) * 100, 2)
-                    if change > 0:
-                        tweet = price + ',' + '(' + locale.currency(change, grouping=True) + ', +' + str(percent) + '%)'
-                        tweetlist.append(tweet)
-                    else:
-                        tweet = price + ',' + '(' + locale.currency(change, grouping=True) + ', ' + str(percent) + '%)'
-                        tweetlist.append(tweet)
-                    break
-                x += 1
-                lp = False
+            add = str(i+1) + '.' + response.json()['data'][i]['symbol'] + ': {}({}, {}%)'.format(locale.currency(cryptoprice, grouping=True), locale.currency(valuechange, grouping = True), round(change, 2)) + '\n'
+
+            if len(tweet+add) > 280:
+                tweet2 = tweet2 + add
+            else:
+                tweet = tweet + add
+    print(tweet)
+    api.update_status(tweet)
+    if tweet2 != '':
+        print(tweet2)
+        api.update_status(tweet2)
     
-    final = ''
-    if lp == True:
-        for tweet in tweetlist:
-            final = final + '\n' + tweet
-        #print(final)
-        time.sleep(360)
-        continue
-    else:
-        a = latestprice
-        for tweet in tweetlist:
-            final = final + '\n' + tweet
-        print(final)
-        api.update_status(final)
-        time.sleep(3600)
+    time.sleep(1800)
 
-
-# In[ ]:
+    tweet = 'Market Cap:\n\n'
+    def human_format(num):
+        num = float('{:.5g}'.format(num))
+        magnitude = 0
+        while abs(num) >= 1000:
+            magnitude += 1
+            num /= 1000.0
+        return '${} {}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'Mn', 'Bn', 'Tn'][magnitude])
+    for i in range(10):
+        tweet = tweet + str(i+1) + '.' + response.json()['data'][i]['name'] + ': ' + human_format(response.json()['data'][i]['quote']['USD']['market_cap']) + '\n'
+    print(tweet)
+    api.update_status(tweet)
+    time.sleep(1800)
 
 
 
